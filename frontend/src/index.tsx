155060fde5712e56184dd418fdf363fb36cf4865
양성훈 <didtjdgns852@naver.com>
2022-11-14 16:42:21 +0900

    chore: fe - 전역상태관리 recoil 설치 및 설정 #2

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
 	</React.StrictMode>
 );
 
