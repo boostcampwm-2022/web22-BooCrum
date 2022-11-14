2d692b633f470d85699d649a3d4ddf1668075207
양성훈 <didtjdgns852@naver.com>
2022-11-14 16:51:34 +0900

    chore: fe - styled-component 설정 #2

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
+import { GlobalStyle } from './global.style';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<RecoilRoot>
+			<GlobalStyle />
 			<App />
 		</RecoilRoot>
 	</React.StrictMode>
