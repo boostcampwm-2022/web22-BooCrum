841cb849b73db30878220e6576a4cb91d2d60b63
Jinwoo Lee <binarywooo@gmail.com>
2022-11-14 17:11:15 +0900

    chore: be - 개발환경 초기 세팅 #3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: process.env.NODE_ENV === 'develop',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import session from 'express-session';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const MySQLStore = require('express-mysql-session')(session);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Session 초기화 코드 =====================
+  // const MySqlStorage = MySQLStore(session);
+  // const sqlStorage = new MySqlStorage({
+  //   host: process.env.MYSQL_HOST,
+  //   port: parseInt(process.env.MYSQL_PORT),
+  //   user: process.env.MYSQL_USERNAME,
+  //   password: process.env.MYSQL_PASSWORD,
+  //   database: process.env.MYSQL_DATABASE,
+  // });
+  // app.use(
+  //   session({
+  //     secret: 'secret',
+  //     resave: false,
+  //     saveUninitialized: false,
+  //     store: sqlStorage,
+  //     cookie: {
+  //       maxAge: 6000 * 24 * 365,
+  //       httpOnly: true,
+  //     },
+  //   }),
+  // );
+  //==========================================
+
   await app.listen(3000);
 }
 bootstrap();
