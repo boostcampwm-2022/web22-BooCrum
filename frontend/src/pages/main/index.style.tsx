632684ea494ddc3f47f13a2c6b5416e0473d7cc8
juhyojeong <juhyojeong58@gmail.com>
2022-11-15 10:31:00 +0900

    refactor: fe - index style 분리 #10

---
+++ frontend/src/pages/main/index.style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	display: flex;
+`;
+export const Section = styled.div`
+	flex-grow: 1;
+`;
--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from './header.component';
+import { Section, Wrapper } from './index.style';
 import Sidebar from './sidebar.component';
 
 function Main() {
@@ -9,17 +9,9 @@ function Main() {
 			<Sidebar />
 			<Section>
 				<Header />
-				main page
 			</Section>
 		</Wrapper>
 	);
 }
 
 export default Main;
-
-const Wrapper = styled.div`
-	display: flex;
-`;
-const Section = styled.div`
-	flex-grow: 1;
-`;
