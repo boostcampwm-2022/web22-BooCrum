b6333debd5071d281af53e3ea306ecdf23eac202
tori209 <kyoungho2018@gist.ac.kr>
2022-11-17 00:21:46 +0900

    feat: be - 워크스페이스 생성/조회 기능 추가 #8
    - 워크스페이스 생성 가능
    - 개인이 소유한 워크스페이스 조회 가능
    - 팀이 소유한 워크스페이스 조회 가능
    - 워크스페이스에 참여한 유저 목록 조회 가능
    - class-transformer를 통한 validation 체크를 도입

--- backend/src/workspace/workspace.controller.ts
+++ backend/src/workspace/workspace.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  Controller,
+  Session,
+  Param,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthorizationGuard } from 'src/auth/guard/session.guard';
+import { WorkspaceCreateRequestDto } from './dto/workspaceCreateRequest.dto';
+import { Workspace } from './entity/workspace.entity';
+import { WorkspaceService } from './workspace.service';
 
 @Controller('workspace')
-export class WorkspaceController {}
+export class WorkspaceController {
+  constructor(private workspaceService: WorkspaceService) {}
+
+  @UseGuards(AuthorizationGuard)
+  @Post()
+  async createWorkspace(
+    @Body(new ValidationPipe()) body: WorkspaceCreateRequestDto,
+    @Session() session: Record<string, any>,
+  ): Promise<Workspace> {
+    body.ownerId = session.user.userId;
+    return this.workspaceService.createWorkspace(body);
+  }
+
+  // 유저가 접근 가능한 Workspace 조회 서비스 기능 테스트 목적 라우트입니다.
+  // @Get('/test/user')
+  // async getTest(@Session() session: Record<string, any>) {
+  //   return await this.workspaceService.getUserOwnWorkspaceList(
+  //     session.user.userId,
+  //   );
+  // }
+
+  // 팀 소유의 Workspace 조회 서비스 기능 테스트 목적 라우트입니다.
+  // @Get('/test/team/:teamId')
+  // async getTeamWorkspaceForTest(@Param('teamId') teamId: number) {
+  //   return await this.workspaceService.getTeamOwnWorkspaceList(teamId);
+  // }
+
+  @Get(':workspaceId/participant')
+  async getWorkspaceParticipantList(@Param('workspaceId') workspaceId: string) {
+    return await this.workspaceService.getWorkspaceParticipantList(workspaceId);
+  }
+
+  @Get(':workspaceId')
+  async getWorkspaceData(@Param('workspaceId') workspaceId: string) {
+    return await this.workspaceService.getWorkspaceData(workspaceId);
+  }
+}
--- backend/src/workspace/workspace.service.ts
+++ backend/src/workspace/workspace.service.ts
@@ -1,4 +1,131 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Team } from 'src/team/entity/team.entity';
+import { User } from 'src/user/entity/user.entity';
+import { Repository, DataSource } from 'typeorm';
+import { WorkspaceCreateRequestDto } from './dto/workspaceCreateRequest.dto';
+import { WorkspaceMember } from './entity/workspace-member.entity';
+import { Workspace } from './entity/workspace.entity';
 
 @Injectable()
-export class WorkspaceService {}
+export class WorkspaceService {
+  constructor(
+    @InjectRepository(Workspace)
+    private workspaceRepository: Repository<Workspace>,
+    @InjectRepository(WorkspaceMember)
+    private workspaceMemberRepository: Repository<WorkspaceMember>,
+    private dataSource: DataSource,
+  ) {}
+
+  /**
+   * 워크스페이스를 생성합니다.
+   * @param param0 워크스페이스를 생성하는데 필요한 정보들입니다. (신규 워크스페이스를 소유할 팀/유저 ID와 워크스페이스 이름, 설명)
+   * @returns 생성한 워크스페이스의 정보를 반환합니다.
+   */
+  async createWorkspace({
+    teamId,
+    ownerId: userId,
+    name,
+    description,
+  }: WorkspaceCreateRequestDto): Promise<Workspace> {
+    const newWorkspace = new Workspace();
+    const workspaceMember = new WorkspaceMember();
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.startTransaction();
+
+    try {
+      const teamFind = await queryRunner.manager.findOne(Team, {
+        where: { teamId },
+      });
+      const userFind = await queryRunner.manager.findOne(User, {
+        where: { userId },
+      });
+      if (!teamFind || !userFind) {
+        await queryRunner.rollbackTransaction();
+        await queryRunner.release();
+        throw new BadRequestException('잘못된 사용자 ID 혹은 팀 ID 입니다.');
+      }
+
+      workspaceMember.role = 2;
+      workspaceMember.user = userFind;
+      workspaceMember.workspace = newWorkspace;
+
+      newWorkspace.team = teamFind;
+      newWorkspace.name = name;
+      newWorkspace.description = description ?? null;
+      newWorkspace.team = teamFind;
+
+      const ret = await queryRunner.manager.save(newWorkspace);
+      await queryRunner.manager.save(workspaceMember);
+
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      return ret;
+    } catch (e) {
+      console.error(e);
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      throw e; // 서버 오류니까 되던지기 한다.
+    }
+  }
+
+  /**
+   * 특정 워크스페이스에 대한 모든 정보를 가져옵니다. (소유 팀 + 워크스페이즈 참여자들 + 메타데이터)
+   * @param workspaceId 특정 워크스페이스에 대한 ID 값입니다.
+   * @return 특정 워크스페이스에 대한 모든 정보를 반환합니다.
+   */
+  async getWorkspaceData(workspaceId: string): Promise<Workspace> {
+    return await this.workspaceRepository
+      .createQueryBuilder('ws')
+      .where('ws.workspace_id = :id', { id: workspaceId })
+      .leftJoinAndSelect('ws.workspaceMember', 'wm')
+      .leftJoinAndSelect('wm.user', 'user')
+      .select()
+      .getOne();
+  }
+
+  /**
+   * 특정 워크스페이스에 한번이라도 참여한 사람들의 명단을 불러옵니다.
+   * @param workspaceId 특정 워크스페이스에 대한 ID 값입니다.
+   * @returns 특정 워크스페이스에 참여한 유저들의 명단입니다.
+   */
+  async getWorkspaceParticipantList(
+    workspaceId: string,
+  ): Promise<WorkspaceMember[]> {
+    return await this.workspaceMemberRepository
+      .createQueryBuilder('wm')
+      .where('wm.workspace_id = :id', { id: workspaceId })
+      .leftJoinAndSelect('wm.user', 'user')
+      .getMany();
+  }
+
+  /**
+   * 특정 유저가 참여한 Workspace 목록을 가져옵니다.
+   * @param userId 유저 ID 입니다.
+   * @returns Workspace 목록을 가져옵니다.
+   */
+  async getUserOwnWorkspaceList(userId: string): Promise<WorkspaceMember[]> {
+    return await this.workspaceMemberRepository
+      .createQueryBuilder('wm')
+      .where('wm.user_id = :id', { id: userId })
+      .leftJoinAndSelect('wm.workspace', 'ws')
+      .select(['wm.role', 'ws'])
+      .getMany();
+  }
+
+  /**
+   * 특정 팀이 소유한 Workspace 목록을 가져옵니다.
+   * @param teamId 팀 ID 입니다.
+   * @returns Workspace 목록을 가져옵니다.
+   */
+  async getTeamOwnWorkspaceList(teamId: number): Promise<Workspace[]> {
+    return await this.workspaceRepository
+      .createQueryBuilder('ws')
+      .leftJoin('ws.team', 'team')
+      .where('team.team_id = :id', { id: teamId })
+      .getMany();
+  }
+}
