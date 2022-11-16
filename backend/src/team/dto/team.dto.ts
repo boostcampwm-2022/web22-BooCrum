d25343ef2e6da101e5ac5e4ecd5d4d679c469f02
Jinwoo Lee <binarywooo@gmail.com>
2022-11-17 03:51:56 +0900

    refactor: be - TeamDTO 최대 입력 글자 수 제한 #6

--- backend/src/team/dto/team.dto.ts
+++ backend/src/team/dto/team.dto.ts
@@ -1,12 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, isString, MaxLength } from 'class-validator';
 
 // 팀 생성 시 팀명과 userId를 필수로 받아와야 하므로 DTO 생성하였음
 export class TeamDTO {
   @IsNotEmpty()
+  @MaxLength(50)
   userId: string;
 
   @IsNotEmpty()
+  @MaxLength(50)
   name: string;
 
+  @MaxLength(1024)
   description: string;
 }
