b5c326f6f98930327d91938a5bdc5246a6c078c4
양성훈 <didtjdgns852@naver.com>
2022-11-16 20:20:54 +0900

    chore: fe - 로그인페이지 라우터 연결 #32

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Main from '@pages/main';
 import Error from '@pages/error';
 import Workspace from '@pages/workspace';
+import Login from '@pages/login';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Main />} />
+			<Route path="/login" element={<Login />} />
 			<Route path="/workspace" element={<Workspace />} />
 			<Route path="/*" element={<Error />} />
 		</Routes>
