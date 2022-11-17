360ee86369052f28d85246f8e30ce0dd7ecff63d
양성훈 <didtjdgns852@naver.com>
2022-11-17 15:38:57 +0900

    feat: fe - 유저데이터 mock api 연동 #34

--- frontend/src/components/user-profile/index.tsx
+++ frontend/src/components/user-profile/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Wrapper, ProfileContainer, ProfileItem } from './index.style';
 import userProfileIcon from '@assets/icon/user-profile.svg';
 import logoutIcon from '@assets/icon/logout.svg';
 import useContextMenu from '@hooks/useContextMenu';
+import axios from 'axios';
+
+async function fetchMockUser(): Promise<{ nickname: string }> {
+	const result = await axios.get('https://7f09d24e-a8d4-4e68-a7c5-ec8c6da7ef40.mock.pstmn.io/user/info/profile');
+	return result.data;
+}
 
 function UserProfile() {
+	const [nickName, setNickName] = useState<string>('');
+
 	const { isOpen, menuRef, openContextMenu } = useContextMenu();
+
+	useEffect(() => {
+		async function setUserData() {
+			const { nickname } = await fetchMockUser();
+			setNickName(nickname);
+		}
+		setUserData();
+	}, []);
 
 	return (
 		<Wrapper onClick={openContextMenu}>
@@ -12,7 +29,7 @@ function UserProfile() {
 			<ProfileContainer ref={menuRef} isShow={isOpen}>
 				<ProfileItem>
 					<img src={userProfileIcon} className="icon" alt="profile-icon" />
-					<div className="text">example@example.com</div>
+					<div className="text">{nickName}</div>
 				</ProfileItem>
 				<ProfileItem>
 					<img src={logoutIcon} className="icon" alt="logout-icon" />
