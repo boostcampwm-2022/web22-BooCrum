cb8312e2e781188039f9c44b8d53fc29d7a30d07
juhyojeong <juhyojeong58@gmail.com>
2022-11-18 12:41:39 +0900

    refactor: fe - useDropdown 삭제 #15

--- frontend/src/pages/main/order-dropdown/index.tsx
+++ frontend/src/pages/main/order-dropdown/index.tsx
@@ -2,26 +2,26 @@ import { workspaceOrderState } from '@context/main-workspace';
 import { orderItems } from '@data/workspace-order';
 import { useRecoilState } from 'recoil';
 import { Dropdown, Description } from './index.style';
-import useDropdown from '@hooks/useDropdown';
+import useModal from '@hooks/useModal';
 import dropdownActive from '@assets/icon/dropdown-active.svg';
 import dropdownInActive from '@assets/icon/dropdown-inactive.svg';
 
 function OrderDropdown() {
 	const [orderType, setOrderType] = useRecoilState(workspaceOrderState);
-	const { dropdownRef, isActive, toggleActive } = useDropdown();
+	const { modalRef, isOpenModal, toggleOpenModal } = useModal();
 
 	const handleOrderType = (id: number) => {
 		setOrderType(id);
-		toggleActive();
+		toggleOpenModal();
 	};
 
 	return (
-		<Dropdown ref={dropdownRef}>
-			<div className="dropdown-button" onClick={toggleActive}>
+		<Dropdown ref={modalRef}>
+			<div className="dropdown-button" onClick={toggleOpenModal}>
 				<p>{orderItems[orderType].description}</p>
-				<img className="dropdown-icon" alt="dropdown button" src={isActive ? dropdownActive : dropdownInActive} />
+				<img className="dropdown-icon" alt="dropdown button" src={isOpenModal ? dropdownActive : dropdownInActive} />
 			</div>
-			{isActive && (
+			{isOpenModal && (
 				<div className="dropdown-container">
 					{orderItems.map((type) => (
 						<Description key={type.id} isSelected={orderType === type.id} onClick={() => handleOrderType(type.id)}>
