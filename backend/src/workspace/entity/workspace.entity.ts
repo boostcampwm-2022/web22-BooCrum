6926ff52c9f4e468b3c633d02f62d55a1802efa9
tori209 <kyoungho2018@gist.ac.kr>
2022-11-21 15:13:33 +0900

    fix: be - 워크스페이스에 섬네일 Url 추가 #60

--- backend/src/workspace/entity/workspace.entity.ts
+++ backend/src/workspace/entity/workspace.entity.ts
@@ -33,6 +33,14 @@ export class Workspace {
   })
   updateDate: number;
 
+  @Column({
+    name: 'thumbnail',
+    type: 'varchar',
+    length: 2083, // 참고: http://daplus.net/sql-url%EC%97%90-%EA%B0%80%EC%9E%A5-%EC%A0%81%ED%95%A9%ED%95%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4-%ED%95%84%EB%93%9C-%EC%9C%A0%ED%98%95/
+    nullable: true,
+  })
+  thumbnailUrl: string;
+
   @OneToMany(() => WorkspaceMember, (workspaceMember) => workspaceMember.workspace)
   workspaceMember: WorkspaceMember[];
 }
