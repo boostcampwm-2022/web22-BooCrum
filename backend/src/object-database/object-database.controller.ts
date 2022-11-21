934037cbbcf3f13558cfac7aa03b4c8a267c3757
tori209 <kyoungho2018@gist.ac.kr>
2022-11-22 00:41:03 +0900

    feat: be - Object Table 존재 유무 판단 기능 구현 #65

--- backend/src/object-database/object-database.controller.ts
+++ backend/src/object-database/object-database.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Delete, Param, ValidationPipe } from '@nestjs/common';
+import { Controller, Res, Get, Post, Delete, Param, ValidationPipe } from '@nestjs/common';
 import { CreateTableRequestDto } from './dto/create-table-request.dto';
 import { ObjectDatabaseService } from './object-database.service';
+import { Response } from 'express';
 
 @Controller('object-database')
 export class ObjectDatabaseController {
@@ -14,5 +15,10 @@ export class ObjectDatabaseController {
   @Delete('/:workspaceId')
   async deleteObjectTable(@Param(new ValidationPipe()) { workspaceId }: CreateTableRequestDto) {
     await this.objectDatabaseService.deleteObjectTable(workspaceId);
+  }
+
+  @Get('/:workspaceId')
+  async workspaceExist(@Param(new ValidationPipe()) { workspaceId }: CreateTableRequestDto, @Res() res: Response) {
+    return res.sendStatus((await this.objectDatabaseService.isObjectTableExist(workspaceId)) ? 200 : 404);
   }
 }
--- backend/src/object-database/object-database.service.ts
+++ backend/src/object-database/object-database.service.ts
@@ -1,18 +1,25 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { WorkspaceService } from 'src/workspace/workspace.service';
-import { DataSource, Table } from 'typeorm';
+import { DataSource, Table, QueryRunner } from 'typeorm';
 import { OBJECT_DATABASE_NAME, OBJECT_TABLE_COLUMN_LIST } from './constant/object-database.constant';
 
 @Injectable()
 export class ObjectDatabaseService {
   constructor(private dataSource: DataSource, private workspaceService: WorkspaceService) {}
 
-  async createObjectTable(workspaceId: string) {
+  /**
+   * Object Table을 생성합니다.
+   *
+   * 이미 Object Table을 갖고 있을 경우 무시됩니다.
+   * @param workspaceId 생성할 Object Table와 연결될 Workspace ID
+   */
+  async createObjectTable(workspaceId: string): Promise<void> {
     const workspace = this.workspaceService.getWorkspaceMetadata(workspaceId);
     if (!workspace) throw new BadRequestException('잘못된 워크스페이스 ID 입니다.');
 
     const queryRunner = this.dataSource.createQueryRunner();
     try {
+      await queryRunner.connect();
       await queryRunner.connect();
       await queryRunner.createDatabase(OBJECT_DATABASE_NAME, true);
       await queryRunner.createTable(
@@ -31,7 +38,13 @@ export class ObjectDatabaseService {
     }
   }
 
-  async deleteObjectTable(workspaceId: string) {
+  /**
+   * Object Table을 삭제합니다.
+   *
+   * Object Table이 존재하지 않을 경우 무시됩니다.
+   * @param workspaceId Object Table과 연결된 워크스페이스의 ID
+   */
+  async deleteObjectTable(workspaceId: string): Promise<void> {
     const queryRunner = this.dataSource.createQueryRunner();
     try {
       await queryRunner.connect();
@@ -48,5 +61,23 @@ export class ObjectDatabaseService {
     } finally {
       await queryRunner.release();
     }
+  }
+
+  /**
+   * Object Table 소유 여부를 확인합니다.
+   *
+   * ※주의※ 실제로 해당 Workspace가 존재하는지 확인하지 않습니다.
+   * @param workspaceId 확인할 Workspace의 ID
+   * @returns 존재할 경우 true, 그렇지 아니할 경우 false를 반환합니다.
+   */
+  async isObjectTableExist(workspaceId: string): Promise<boolean> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    return await queryRunner.hasTable(
+      new Table({
+        database: OBJECT_DATABASE_NAME,
+        name: workspaceId,
+      }),
+    );
   }
 }
