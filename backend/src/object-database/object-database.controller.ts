3d6bb3782cad9a5ea671551957642595c3542b17
Jinwoo Lee <binarywooo@gmail.com>
2022-11-22 15:30:35 +0900

    feat: be - 워크스페이스 객체 전체 조회 API 구현 #9

--- backend/src/object-database/object-database.controller.ts
+++ backend/src/object-database/object-database.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Res, Get, Post, Delete, Param, ValidationPipe } from '@nest
 import { CreateTableRequestDto } from './dto/create-table-request.dto';
 import { ObjectDatabaseService } from './object-database.service';
 import { Response } from 'express';
+import { ObjectHandlerService } from './object-handler.service';
 
 @Controller('object-database')
 export class ObjectDatabaseController {
-  constructor(private objectDatabaseService: ObjectDatabaseService) {}
+  constructor(
+    private objectDatabaseService: ObjectDatabaseService,
+    private objectHandlerService: ObjectHandlerService,
+  ) {}
 
   @Post('/:workspaceId')
   async createObjectTable(@Param(new ValidationPipe()) { workspaceId }: CreateTableRequestDto) {
@@ -20,5 +24,10 @@ export class ObjectDatabaseController {
   @Get('/:workspaceId')
   async workspaceExist(@Param(new ValidationPipe()) { workspaceId }: CreateTableRequestDto, @Res() res: Response) {
     return res.sendStatus((await this.objectDatabaseService.isObjectTableExist(workspaceId)) ? 200 : 404);
+  }
+
+  @Get('/:workspaceId/objects')
+  async selectAllObjects(@Param(new ValidationPipe()) { workspaceId }: CreateTableRequestDto) {
+    return this.objectHandlerService.selectAllObjects(workspaceId);
   }
 }
--- backend/src/object-database/object-handler.service.ts
+++ backend/src/object-database/object-handler.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RowDataPacket } from 'mysql2';
+import { WorkspaceService } from 'src/workspace/workspace.service';
+import { DataSource } from 'typeorm';
+import { OBJECT_DATABASE_NAME, OBJECT_TABLE_COLUMN_LIST } from './constant/object-database.constant';
+import { ObjectDatabaseService } from './object-database.service';
 
 @Injectable()
-export class ObjectHandlerService {}
+export class ObjectHandlerService {
+  constructor(private dataSource: DataSource, private objectDatabaseService: ObjectDatabaseService) {}
+
+  async selectAllObjects(workspaceId: string): Promise<RowDataPacket[] | undefined> {
+    const objectTable = this.objectDatabaseService.isObjectTableExist(workspaceId);
+    if (!objectTable) throw new BadRequestException('잘못된 워크스페이스 ID 입니다.');
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      return await queryRunner.query(`SELECT * FROM \`${OBJECT_DATABASE_NAME}\`.\`${workspaceId}\``);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
