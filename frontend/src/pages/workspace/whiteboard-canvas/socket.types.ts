fc373a462d76ac501b67092938263ec76007e7a3
양성훈 <didtjdgns852@naver.com>
2022-11-24 00:49:40 +0900

    feat: fe - socket 초기화 및 connection #82

--- frontend/src/pages/workspace/whiteboard-canvas/socket.types.ts
+++ frontend/src/pages/workspace/whiteboard-canvas/socket.types.ts
@@ -2,7 +2,7 @@ export interface ServerToClientEvents {
 	connect: () => void;
 	disconnect: () => void;
 	init: (arg: AllWorkspaceData) => void;
-	enter_user: (arg: { userData: CanvasObject }) => void;
+	enter_user: (arg: { userData: Member }) => void;
 	leave_user: (arg: { userId: string }) => void;
 	move_pointer: (arg: UserMousePointer) => void;
 	select_object: (arg: { userId: string; objectId: string }) => void;
@@ -21,18 +21,18 @@ export interface ClientToServerEvents {
 	update_object: (arg: UpdatedObject) => void;
 }
 
-interface AllWorkspaceData {
+export interface AllWorkspaceData {
 	members: Member[];
 	objects: CanvasObject[];
 }
 
-interface Member {
+export interface Member {
 	userId: string;
 	nickname: string;
 	color: string;
 }
 
-interface CanvasObject {
+export interface CanvasObject {
 	type: 'postit' | 'section' | 'draw';
 	objectId: string;
 	x: number;
@@ -44,16 +44,16 @@ interface CanvasObject {
 	text?: string;
 }
 
-interface MousePointer {
+export interface MousePointer {
 	x: number;
 	y: number;
 }
 
-interface UserMousePointer extends MousePointer {
+export interface UserMousePointer extends MousePointer {
 	userId: string;
 }
 
-interface UpdatedObject {
+export interface UpdatedObject {
 	objectId: string;
 	x?: number;
 	y?: number;
---
+++ frontend/src/pages/workspace/whiteboard-canvas/useSocket.ts
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { io, Socket } from 'socket.io-client';
+import { ClientToServerEvents, Member, ServerToClientEvents } from '@pages/workspace/whiteboard-canvas/socket.types';
+
+function useSocket(canvas: fabric.Canvas | null) {
+	const [isConnected, setIsConnected] = useState(false);
+	const [members, setMembers] = useState<Member[]>([]);
+	const {
+		state: { workspaceId },
+	} = useLocation();
+	const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(`/workspace/${workspaceId}`);
+
+	useEffect(() => {
+		socket.on('connect', () => {
+			setIsConnected(true);
+		});
+
+		socket.on('disconnect', () => {
+			setIsConnected(false);
+		});
+
+		socket.on('init', ({ members, objects }) => {
+			setMembers(members);
+			//todo: objects 업데이트
+		});
+
+		socket.on('enter_user', ({ userData }) => {
+			setMembers((prev) => [...prev, userData]);
+		});
+
+		socket.on('leave_user', ({ userId }) => {
+			setMembers((prev) => prev.filter((user) => user.userId !== userId));
+		});
+
+		socket.on('move_pointer', ({ userId, x, y }) => {
+			// todo move_pointer 업데이트
+		});
+
+		socket.on('select_object', ({ userId, objectId }) => {
+			//todo select 업데아트
+		});
+
+		socket.on('unselect_object', ({ userId, objectId }) => {
+			//todo unselect 업데아트
+		});
+
+		socket.on('create_object', ({ objectData }) => {
+			//todo object 추가
+		});
+
+		socket.on('delete_object', ({ objectId }) => {
+			// todo object 삭제
+		});
+
+		socket.on('update_object', ({ objectData }) => {
+			//todo object 업데이트
+		});
+	}, []);
+
+	return {
+		isConnected,
+		socket,
+		members,
+	};
+}
+
+export default useSocket;
