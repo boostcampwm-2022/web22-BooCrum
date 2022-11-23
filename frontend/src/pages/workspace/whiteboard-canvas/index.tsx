12915eaa8a884daec4b25f1dfb96b4f50236eed9
양성훈 <didtjdgns852@naver.com>
2022-11-23 21:46:47 +0900

    refactor: fe - cavnas hook 분리 #82

--- frontend/src/pages/workspace/whiteboard-canvas/index.tsx
+++ frontend/src/pages/workspace/whiteboard-canvas/index.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
-import { drawingGird } from '@utils/fabric.utils';
+import useCanvas from './useCanvas';
 
 function WhiteboardCanvas() {
-	const canvas = useRef<fabric.Canvas | null>(null);
-	useEffect(() => {
-		canvas.current = initCanvas();
-		return () => {
-			if (canvas.current) {
-				canvas.current.dispose();
-				canvas.current = null;
-			}
-		};
-	}, []);
-
-	const initCanvas = () => {
-		const grid = 50;
-		const canvasWidth = 2000;
-		const canvasHeight = 900;
-
-		const fabricCanvas = new fabric.Canvas('canvas', {
-			height: canvasHeight,
-			width: canvasWidth,
-			backgroundColor: '#f1f1f1',
-		});
-
-		drawingGird(fabricCanvas, canvasWidth, canvasHeight, grid);
-
-		// 이벤트 추가 예시
-		fabricCanvas.on('object:added', (e) => {
-			console.log(e);
-		});
-
-		return fabricCanvas;
-	};
+	const { canvas } = useCanvas();
 
 	// Object 추가 예시
 	const addObj = () => {
-		if (!canvas.current) return;
+		if (!canvas) return;
 		const rect = new fabric.Rect({
 			height: 280,
 			width: 200,
@@ -45,14 +14,14 @@ function WhiteboardCanvas() {
 			left: 100,
 			fill: 'yellow',
 		});
-		canvas.current.add(rect);
+		canvas.add(rect);
 	};
 	// Object 삭제 예시
 	const clearObjects = () => {
-		if (!canvas.current) return;
-		canvas.current.forEachObject((obj) => {
+		if (!canvas) return;
+		canvas.forEachObject((obj) => {
 			// 그리드 제외 하고 삭제
-			if (!(obj instanceof fabric.Line)) canvas.current?.remove(obj);
+			if (!(obj instanceof fabric.Line)) canvas?.remove(obj);
 		});
 	};
 	return (
---
+++ frontend/src/pages/workspace/whiteboard-canvas/useCanvas.ts
@@ -0,0 +1,44 @@
+import { useRef, useEffect } from 'react';
+import { fabric } from 'fabric';
+import { drawingGird } from '@utils/fabric.utils';
+
+function useCanvas() {
+	const canvas = useRef<fabric.Canvas | null>(null);
+
+	useEffect(() => {
+		canvas.current = initCanvas();
+		return () => {
+			if (canvas.current) {
+				canvas.current.dispose();
+				canvas.current = null;
+			}
+		};
+	}, []);
+
+	const initCanvas = () => {
+		const grid = 50;
+		const canvasWidth = 2000;
+		const canvasHeight = 900;
+
+		const fabricCanvas = new fabric.Canvas('canvas', {
+			height: canvasHeight,
+			width: canvasWidth,
+			backgroundColor: '#f1f1f1',
+		});
+
+		drawingGird(fabricCanvas, canvasWidth, canvasHeight, grid);
+
+		// 이벤트 추가 예시
+		fabricCanvas.on('object:added', (e) => {
+			console.log(e);
+		});
+
+		return fabricCanvas;
+	};
+
+	return {
+		canvas: canvas.current,
+	};
+}
+
+export default useCanvas;
