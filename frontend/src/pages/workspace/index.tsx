e55b4bff4d034afb9e5c2683162bb9e6b4bd7bf1
juhyojeong <juhyojeong58@gmail.com>
2022-11-23 16:38:17 +0900

    chore: fe - edit context menu 적용 #64

--- frontend/src/pages/workspace/index.tsx
+++ frontend/src/pages/workspace/index.tsx
@@ -1,16 +1,31 @@
+import ContextMenu from '@components/context-menu';
+import useContextMenu from '@hooks/useContextMenu';
 import { useLocation } from 'react-router-dom';
 import WhiteboardCanvas from './whiteboard-canvas';
 import Layout from './layout';
+import ObjectEditMenu from './objectEditMenu';
 
 function Workspace() {
 	const {
 		state: { name, workspaceId },
 	} = useLocation();
+	const { isOpen, menuRef, toggleOpen, menuPosition } = useContextMenu(); // whiteboard로 빼기
+
+	const openContextMenu: React.MouseEventHandler<HTMLDivElement> = (e) => {
+		e.preventDefault();
+		toggleOpen(e.clientX, e.clientY);
+	};
 
 	return (
 		<>
 			<Layout name={name} />
 			<WhiteboardCanvas></WhiteboardCanvas>
+
+			<div style={{ width: '100vw', height: '100vh' }} onClick={openContextMenu}></div>
+
+			<ContextMenu isOpen={isOpen} menuRef={menuRef} posX={menuPosition.x} posY={menuPosition.y}>
+				<ObjectEditMenu selectedObject="section" />
+			</ContextMenu>
 		</>
 	);
 }
