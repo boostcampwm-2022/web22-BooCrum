8c5b42cf3609ba59c9e201e35f110753da27c181
juhyojeong <juhyojeong58@gmail.com>
2022-11-23 14:45:26 +0900

    feat: fe - section edit menu 구현 #64

--- frontend/src/pages/workspace/objectEditMenu/index.style.tsx
+++ frontend/src/pages/workspace/objectEditMenu/index.style.tsx
@@ -1,3 +1,73 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	width: 80px;
+	height: 32px;
+
+	position: relative;
+
+	display: flex;
+	padding: 0px 10px;
+
+	border-radius: 4px;
+
+	background: ${({ theme }) => theme.gray_4};
+`;
+
+export const ColorSelect = styled.div<{ color: string }>`
+	display: flex;
+	align-items: center;
+
+	cursor: pointer;
+
+	padding-right: 6px;
+	border-right: 1px solid ${({ theme }) => theme.gray_3};
+
+	.selected-color {
+		background: ${({ color }) => color};
+		border-radius: 30px;
+
+		width: 16px;
+		height: 16px;
+
+		margin-right: 4px;
+	}
+`;
+
+export const Rename = styled.div<{ selected: boolean }>`
+	background: ${({ selected, theme }) => (selected ? theme.blue_2 : 'transparent')};
+
+	margin-left: 6px;
+	padding: 0 4px;
+
+	display: flex;
+	align-items: center;
+
+	cursor: pointer;
+`;
+
+export const ColorChip = styled.div`
+	padding: 8px 16px;
+	border-radius: 10px;
+
+	background: ${({ theme }) => theme.gray_4};
+
+	position: absolute;
+	left: -50px;
+	top: -42px;
+
+	display: flex;
+
+	.color {
+		width: 16px;
+		height: 16px;
+
+		border-radius: 30px;
+
+		cursor: pointer;
+
+		+ .color {
+			margin-left: 4px;
+		}
+	}
+`;
--- frontend/src/pages/workspace/objectEditMenu/index.tsx
+++ frontend/src/pages/workspace/objectEditMenu/index.tsx
@@ -1,7 +1,46 @@
-import { Container } from './index.style';
+import { colorChips } from '@data/workspace-object-color';
+import { useState } from 'react';
+import { ColorChip, ColorSelect, Container, Rename } from './index.style';
+import dropdownColor from '@assets/icon/dropdown-color.svg';
+import { ReactComponent as RenameSection } from '@assets/icon/rename-section.svg';
+
+const selectedType: { [index: string]: number } = {
+	NONE: 0,
+	COLOR: 1,
+	RENAME: 2,
+};
 
 function ObjectEditMenu() {
-	return <Container>edit</Container>;
+	const [color, setColor] = useState(colorChips[0]); // 나중에 밖으로 빼기
+	const [selected, setSelected] = useState(selectedType.NONE);
+
+	const handleColor = (color: string) => {
+		setColor(color);
+		setSelected(selectedType.NONE);
+	};
+
+	const toggleColorSelect = () => {
+		setSelected((prevSelect) => (prevSelect === selectedType.COLOR ? selectedType.NONE : selectedType.COLOR));
+	};
+
+	return (
+		<Container>
+			<ColorSelect color={color} onClick={toggleColorSelect}>
+				<div className="selected-color" />
+				<img alt="set color" className="dropdown-color" src={dropdownColor} />
+			</ColorSelect>
+			<Rename selected={selected === selectedType.RENAME} onClick={() => setSelected(selectedType.RENAME)}>
+				<RenameSection stroke={selected === selectedType.RENAME ? 'white' : '#777777'} />
+			</Rename>
+			{selected === selectedType.COLOR && (
+				<ColorChip>
+					{colorChips.map((color) => (
+						<div key={color} className="color" style={{ background: color }} onClick={() => handleColor(color)} />
+					))}
+				</ColorChip>
+			)}
+		</Container>
+	);
 }
 
 export default ObjectEditMenu;
