b6381461785dbc2e6ea2c6aca5d4065e0413e1b6
thingseong <audtjd1893@gmail.com>
2022-11-29 00:42:41 +0900

    feat: fe - 오브젝트 삭제시 editmenu 닫는 기능 추가 #126

--- frontend/src/pages/workspace/whiteboard-canvas/useEditMenu.ts
+++ frontend/src/pages/workspace/whiteboard-canvas/useEditMenu.ts
@@ -12,11 +12,13 @@ function useEditMenu(canvas: React.MutableRefObject<fabric.Canvas | null>) {
 		canvas.current.on('mouse:down', handleOutsideClick);
 		canvas.current.on('object:modified', handleMenuPosition);
 		canvas.current.on('mouse:wheel', handleMenuPosition);
+		canvas.current.on('object:removed', handleRemoveObject);
 
 		return () => {
 			canvas.current?.off('mouse:down', handleOutsideClick);
 			canvas.current?.off('object:modified', handleMenuPosition);
 			canvas.current?.off('mouse:wheel', handleMenuPosition);
+			canvas.current?.off('object:removed', handleRemoveObject);
 		};
 	}, [isOpen]);
 
@@ -27,7 +29,6 @@ function useEditMenu(canvas: React.MutableRefObject<fabric.Canvas | null>) {
 		const left = coord ? (coord[0].x + coord[1].x) / 2 - 30 : 0;
 
 		setIsOpen(true);
-		console.log(top, left);
 		setMenuPosition({ x: left, y: top });
 	};
 
@@ -50,6 +51,10 @@ function useEditMenu(canvas: React.MutableRefObject<fabric.Canvas | null>) {
 		const top = coord ? coord[0].y - 40 : 0;
 		const left = coord ? (coord[0].x + coord[1].x) / 2 - 30 : 0;
 		setMenuPosition({ x: left, y: top });
+	};
+	const handleRemoveObject = (opt: fabric.IEvent) => {
+		if (!isOpen) return;
+		setIsOpen(false);
 	};
 
 	return { isOpen, menuRef, openMenu, menuPosition };
