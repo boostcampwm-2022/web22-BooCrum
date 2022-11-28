d6f5a4fe131b156ae3f64fb59e6ddb62214be548
juhyojeong <juhyojeong58@gmail.com>
2022-11-28 13:58:52 +0900

    feat: fe - workspace url 설정 #116

--- frontend/src/pages/main/workspace-card/index.tsx
+++ frontend/src/pages/main/workspace-card/index.tsx
@@ -1,17 +1,25 @@
 import ContextMenu from '@components/context-menu';
 import useContextMenu from '@hooks/useContextMenu';
+import { useNavigate } from 'react-router-dom';
 import WorkspaceMenu from '../workspace-menu';
 import { CardLayout } from './index.style';
 import { WorkspaceCardProps } from './index.type';
 
 function WorkspaceCard({ workspaceId, role, title, timestamp, imgSrc, setWorkspaceList }: WorkspaceCardProps) {
 	const { isOpen, menuRef, toggleOpen, menuPosition } = useContextMenu();
+	const navigate = useNavigate();
+
 	const openContextMenu: React.MouseEventHandler<HTMLDivElement> = (e) => {
 		e.preventDefault();
 		toggleOpen(e.pageX, e.pageY);
 	};
+
+	const handleRouting = () => {
+		navigate(`/workspace/${workspaceId}`, { state: { workspaceId, name: title } });
+	};
+
 	return (
-		<>
+		<div onClick={handleRouting}>
 			<CardLayout onContextMenu={openContextMenu}>
 				<img className="card-thumbnail" src={imgSrc}></img>
 				<div className="card-info">
@@ -27,7 +35,7 @@ function WorkspaceCard({ workspaceId, role, title, timestamp, imgSrc, setWorkspa
 					setWorkspaceList={setWorkspaceList}
 				></WorkspaceMenu>
 			</ContextMenu>
-		</>
+		</div>
 	);
 }
 
