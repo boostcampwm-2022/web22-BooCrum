ffb18c18dc3a8b4e9ab925cc6eed5dd343ca00e5
juhyojeong <juhyojeong58@gmail.com>
2022-11-28 15:09:35 +0900

    feat: fe - share modal 마크업 #117

--- frontend/src/pages/workspace/share-modal/index.style.tsx
+++ frontend/src/pages/workspace/share-modal/index.style.tsx
@@ -2,13 +2,92 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	position: absolute;
-	width: 400px;
-	height: 200px;
+	z-index: 10;
+	width: 600px;
+	height: 400px;
+
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
-	background: #ffffff;
+
+	background: ${({ theme }) => theme.white};
 	box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
 	border-radius: 8px;
-	z-index: 10;
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		padding: 10px 20px;
+
+		border-bottom: 1px solid ${({ theme }) => theme.gray_1};
+	}
+
+	.title {
+		font-size: 20px;
+		font-weight: 500;
+		line-height: 27px;
+
+		color: ${({ theme }) => theme.black};
+
+		margin: 0;
+	}
+
+	.modal-close {
+		width: 36px;
+		height: 36px;
+
+		cursor: pointer;
+	}
+
+	.bottom {
+		position: absolute;
+		bottom: 0;
+
+		width: 100%;
+		display: flex;
+		align-items: center;
+
+		border-top: 1px solid ${({ theme }) => theme.gray_1};
+
+		font-size: 15px;
+		line-height: 20px;
+		font-weight: 400;
+	}
+
+	.copy-icon {
+		width: 36px;
+		height: 36px;
+
+		cursor: pointer;
+	}
+`;
+
+export const Invite = styled.div<{ isValid: boolean }>`
+	display: flex;
+	justify-content: space-between;
+	padding: 14px;
+
+	.invite-input {
+		outline: none;
+		border: 2px solid ${({ isValid, theme }) => (isValid ? theme.blue_2 : theme.gray_2)};
+		border-radius: 10px;
+
+		padding: 12px 16px;
+		width: 400px;
+	}
+
+	.invite-button {
+		width: 130px;
+
+		border-radius: 10px;
+		background: ${({ isValid, theme }) => (isValid ? theme.blue_2 : theme.gray_2)};
+		color: ${({ theme }) => theme.white};
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		cursor: pointer;
+	}
 `;
--- frontend/src/pages/workspace/share-modal/index.tsx
+++ frontend/src/pages/workspace/share-modal/index.tsx
@@ -1,5 +1,7 @@
-import { RefObject } from 'react';
-import { Container } from './index.style';
+import { RefObject, useState } from 'react';
+import { Container, Invite } from './index.style';
+import closeIcon from '@assets/icon/close.svg';
+import copyLink from '@assets/icon/copy-link.svg';
 
 interface ShareModalProps {
 	id: string;
@@ -8,7 +10,26 @@ interface ShareModalProps {
 }
 
 function ShareModal({ id, modalRef, closeModal }: ShareModalProps) {
-	return <Container ref={modalRef}>share {id}</Container>;
+	const [email, setEmail] = useState('');
+
+	return (
+		<Container ref={modalRef}>
+			<div className="header">
+				<h1 className="title">Invite</h1>
+				<img alt="close modal" src={closeIcon} className="modal-close" onClick={closeModal} />
+			</div>
+
+			<Invite isValid={email !== ''}>
+				<input className="invite-input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				<div className="invite-button">send invite</div>
+			</Invite>
+
+			<div className="bottom">
+				<img alt="copy workspace url" src={copyLink} className="copy-icon" />
+				copy link
+			</div>
+		</Container>
+	);
 }
 
 export default ShareModal;
