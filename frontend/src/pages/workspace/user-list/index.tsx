25c3b463339fbb6fd1f8f1e59d19a0fa1366f248
juhyojeong <juhyojeong58@gmail.com>
2022-11-29 12:23:54 +0900

    chore: fe - 필요없는 코드 삭제 #118

--- frontend/src/pages/workspace/user-list/index.tsx
+++ frontend/src/pages/workspace/user-list/index.tsx
@@ -1,31 +1,17 @@
 import { Container, UserDetail, UserInfo } from './index.style';
-import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { membersState } from '@context/workspace';
 import { Member } from './index.type';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import userIcon from '@assets/icon/user-icon.svg';
-import { userProfileState } from '@context/user';
 
 function UserList() {
 	const members = useRecoilValue(membersState);
-	const userProfileLoadable = useRecoilValueLoadable(userProfileState);
-	const [user, setUser] = useState<Member>({
-		userId: '',
-		nickname: '',
-		color: '',
-	});
 	const [selectedMember, setSelectedMember] = useState<Member>({
 		userId: '',
 		nickname: '',
 		color: '',
 	});
-
-	useEffect(() => {
-		if (userProfileLoadable.state === 'hasValue') {
-			const { userId, nickname } = userProfileLoadable.contents;
-			setUser({ userId, nickname, color: '#d9d9d9' });
-		}
-	}, [userProfileLoadable]);
 
 	const handleDetailUser = (member: Member) => {
 		setSelectedMember(member);
@@ -34,12 +20,6 @@ function UserList() {
 	return (
 		<>
 			<Container>
-				<UserInfo color="#d9d9d9">
-					<div className="user-profile" onClick={() => handleDetailUser(user)}>
-						<img alt="user profile" className="user-icon" src={userIcon} />
-					</div>
-					<UserDetail>{selectedMember.nickname}</UserDetail>
-				</UserInfo>
 				{members.slice(0, 5).map((member) => (
 					<UserInfo key={member.userId} color={member.color}>
 						<div className="user-profile" onClick={() => handleDetailUser(member)}>
