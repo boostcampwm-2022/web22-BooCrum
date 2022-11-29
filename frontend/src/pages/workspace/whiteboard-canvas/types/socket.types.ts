adb526fd30230e100c443ebbcd888c442ce0948b
양성훈 <didtjdgns852@naver.com>
2022-11-29 19:35:09 +0900

    feat: fe - create-object 서버 전송 #111

--- frontend/src/pages/workspace/whiteboard-canvas/types/socket.types.ts
+++ frontend/src/pages/workspace/whiteboard-canvas/types/socket.types.ts
@@ -11,16 +11,17 @@ export interface ServerToClientEvents {
 	move_pointer: (arg: UserMousePointer) => void;
 	select_object: (arg: { userId: string; objectId: string }) => void;
 	unselect_object: (arg: { userId: string; objectId: string }) => void;
-	create_object: (arg: { objectData: ObjectDataFromServer }) => void;
+	create_object: (arg: ObjectDataFromServer) => void;
 	delete_object: (arg: { objectId: string }) => void;
 	update_object: (arg: { objectData: ObjectDataFromServer }) => void;
+	exception: (arg: any) => void;
 }
 
 export interface ClientToServerEvents {
 	move_pointer: (arg: MousePointer) => void;
 	select_object: (arg: { objectId: string }) => void;
 	unselect_object: (arg: { objectId: string }) => void;
-	create_object: (arg: { objectData: ObjectDataToServer }) => void;
+	create_object: (arg: ObjectDataToServer) => void;
 	delete_object: (arg: { objectId: string }) => void;
 	update_object: (arg: ObjectDataToServer) => void;
 }
