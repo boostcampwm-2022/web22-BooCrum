111cf0004eadc924d6deaab3681893b73778c7a6
양성훈 <didtjdgns852@naver.com>
2022-12-08 02:59:54 +0900

    feat: fe - export modal 구현 #230

--- frontend/src/pages/workspace/left-side/index.style.tsx
+++ frontend/src/pages/workspace/left-side/index.style.tsx
@@ -23,28 +23,3 @@ export const Container = styled.div`
 		cursor: pointer;
 	}
 `;
-
-export const ModalContent = styled.div`
-	.export-modal-text {
-		margin-top: 32px;
-		font-size: 20px;
-		margin-left: 16px;
-		font-weight: 400;
-	}
-`;
-
-export const ExportButton = styled.button`
-	position: absolute;
-	bottom: 1em;
-	right: 1em;
-	border: none;
-	background-color: #2071ff;
-	color: white;
-	font-weight: 600;
-	padding: 1em;
-	border-radius: 30px;
-
-	:disabled {
-		background-color: lightgrey;
-	}
-`;
