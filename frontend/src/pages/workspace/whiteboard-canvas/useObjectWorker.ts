6e840fcb59932b07a81b3e7f958f9cfc21a8ec3d
양성훈 <didtjdgns852@naver.com>
2022-12-09 05:32:25 +0900

    fix: fe - object worker script spread 제거 #245

--- frontend/src/pages/workspace/whiteboard-canvas/useObjectWorker.ts
+++ frontend/src/pages/workspace/whiteboard-canvas/useObjectWorker.ts
@@ -11,6 +11,7 @@ function useObjectWorker(
 
 	const initWorker = () => {
 		const code = workerModule.toString();
+		console.log(code);
 		const blob = new Blob([`(${code})()`]);
 		return new Worker(URL.createObjectURL(blob));
 	};
