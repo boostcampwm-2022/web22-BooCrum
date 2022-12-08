8ef665e18ac6001206ccbd36622bd75d2fd92d4f
tori209 <kyoungho2018@gist.ac.kr>
2022-12-08 18:38:43 +0900

    feat: be - 소켓 서버 세션 데이터와 Redis 연결 #237

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createSessionMiddleware } from './middlewares/session.middleware';
+import { RedisIoAdapter } from './socket/adapter/custom-socket.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const redisIoAdapter = new RedisIoAdapter(app);
+
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
 
   // 세션 초기화
   app.use(createSessionMiddleware());
+
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
---
+++ backend/src/socket/adapter/custom-socket.adapter.ts
@@ -0,0 +1,23 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+
+export class RedisIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+
+  async connectToRedis(): Promise<void> {
+    const pubClient = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` });
+    const subClient = pubClient.duplicate();
+
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+
+    this.adapterConstructor = createAdapter(pubClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+}
