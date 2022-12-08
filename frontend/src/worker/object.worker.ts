d7dc2beb7bf2d0b8a8b3d4922acdaaecea7cb232
양성훈 <didtjdgns852@naver.com>
2022-12-09 03:28:37 +0900

    feat: fe - object worker 스크립트 구현 #245

--- frontend/src/worker/object.worker.ts
+++ frontend/src/worker/object.worker.ts
@@ -1,5 +1,36 @@
+import { ObjectDataToServer } from '@pages/workspace/whiteboard-canvas/types';
+
 export default () => {
+	let waitQueue: ObjectDataToServer[] = [];
+
+	self.onmessage = ({ data }: { data: ObjectDataToServer }) => {
+		waitQueue.push(data);
+	};
+
 	setInterval(() => {
-		postMessage(Date.now());
-	}, 1000);
+		const data = batchQueue();
+		if (data) {
+			Object.keys(data).forEach((key) => {
+				self.postMessage(data[key]);
+			});
+		}
+	}, 33.4);
+
+	const batchQueue = () => {
+		if (waitQueue.length === 0) return;
+		const taskQueue = waitQueue;
+		waitQueue = [];
+
+		const dataByObject: {
+			[key: string]: ObjectDataToServer;
+		} = {};
+		taskQueue.forEach((item) => {
+			if (dataByObject.hasOwnProperty(item.objectId)) {
+				dataByObject[item.objectId] = { ...dataByObject[item.objectId], ...item };
+			} else {
+				dataByObject[item.objectId] = { ...item };
+			}
+		});
+		return dataByObject;
+	};
 };
