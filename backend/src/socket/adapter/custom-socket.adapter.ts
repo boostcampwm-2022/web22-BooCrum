87cbf24ae1326161a880f4d997b7c5d335e15d30
tori209 <kyoungho2018@gist.ac.kr>
2022-12-09 20:57:02 +0900

    fix: be - RedisAdapter를 ioredis로 변경 #237

--- backend/src/socket/adapter/custom-socket.adapter.ts
+++ backend/src/socket/adapter/custom-socket.adapter.ts
@@ -1,16 +1,22 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
-import { createClient } from 'redis';
+import { Logger } from '@nestjs/common';
+import { Cluster } from 'ioredis';
 
 export class RedisIoAdapter extends IoAdapter {
+  private logger = new Logger('RedisIoAdapter');
+
   private adapterConstructor: ReturnType<typeof createAdapter>;
 
   async connectToRedis(): Promise<void> {
-    const pubClient = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` });
+    const pubClient = new Cluster([{ host: process.env.REDIS_HOST, port: parseInt(process.env.REDIS_PORT) }], {
+      dnsLookup: (address, callback) => callback(null, address),
+    });
     const subClient = pubClient.duplicate();
 
-    await Promise.all([pubClient.connect(), subClient.connect()]);
+    pubClient.on('error', (err) => this.logger.error(err));
+    subClient.on('error', (err) => this.logger.error(err));
 
     this.adapterConstructor = createAdapter(pubClient, subClient);
   }
